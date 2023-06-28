083d1bc3e72681459e44214dd36b7cf03245be8a
bor1gunbbang <whl564@naver.com>
2023-06-28 15:36:15 +0900

    로그인 메서드 토큰도 추가 완료

--- newposet/src/main/java/com/sparta/newposet/controller/UserController.java
+++ newposet/src/main/java/com/sparta/newposet/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.sparta.newposet.controller;
 
 
+import com.sparta.newposet.dto.LoginRequestDto;
 import com.sparta.newposet.dto.SignupRequestDto;
 import com.sparta.newposet.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,5 +24,16 @@ public class UserController {
     public String signup(SignupRequestDto requestDto){
         userService.signup(requestDto);
         return "회원가입이 완료되었습니다.";
+    }
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res){
+        try {
+            userService.login(requestDto, res); //로그인검증 끝나면jwt토큰 쿠키에 넣고 그 쿠키 담을것 res에
+        } catch (Exception e) {
+           return "로그인 실패 아이디와 암호를 확인하세요";
+        }
+
+
+        return "로그인 완료";
     }
 }
--- newposet/src/main/java/com/sparta/newposet/service/UserService.java
+++ newposet/src/main/java/com/sparta/newposet/service/UserService.java
@@ -1,9 +1,12 @@
 package com.sparta.newposet.service;
 
+import com.sparta.newposet.dto.LoginRequestDto;
 import com.sparta.newposet.dto.SignupRequestDto;
 import com.sparta.newposet.entity.User;
 import com.sparta.newposet.entity.UserRoleEnum;
+import com.sparta.newposet.jwt.JwtUtil;
 import com.sparta.newposet.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +18,11 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final JwtUtil jwtUtil;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     // ADMIN_TOKEN
@@ -43,5 +48,22 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password,role);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password  = requestDto.getPassword();
+        //사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(// 유저네임을 확인한후 있으면 트루를 리턴
+                ()-> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        //비밀번호 확인
+
+        if (!passwordEncoder.matches(password,user.getPassword())){ //비번이 틀리면 밑에가 실행
+            throw new IllegalArgumentException("비밀번호가 일치 하지 않습니다.");
+        }
+        // 인증 완료 JWT생성 후 쿠키에 저장 후 Response객체에 추가
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token,res);
     }
 }
